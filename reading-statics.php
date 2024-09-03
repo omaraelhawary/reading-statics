8b3f0f88d2f1d9a1ecaede6d23153be580df512f
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-03 20:04:52 +0300

    Show plugin settings below WP Dashboard > Settings

--- reading-statics.php
+++ reading-statics.php
@@ -1,9 +1,28 @@
 <?php 
 
 /*
-    Plugin Name: Posts Ready Statics
+    Plugin Name: Posts Read Statics
     Description: This plugin to get read statics for posts.
     Version: 1.0
     Author: Omar ElHawary
     Author URI: https://www.linkedin.com/in/omaraelhawary/
 */
+
+function postsReadStatics(){
+    add_options_page(
+        'Posts Read Statics',
+        'Read Statics',
+        'manage_options',
+        'posts-read-statics',
+        'ourSettignsPageHTML'
+    );
+}
+
+function ourSettignsPageHTML(){  ?>
+
+<h1>Posts Read Statics</h1>
+
+<?php    
+}
+
+add_action('admin_menu', 'postsReadStatics');
