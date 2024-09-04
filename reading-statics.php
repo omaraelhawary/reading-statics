df4237c0d5a6412a54b37e612a9502cdfc704cec
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-04 18:09:10 +0300

    Create Display Location field settings

--- reading-statics.php
+++ reading-statics.php
@@ -11,22 +11,48 @@
 Class ReadingStatics{
     function __construct(){
         add_action('admin_menu', array($this, 'adminPage'));
+        add_action('admin_init', array($this, 'registerSettings'));
     }
-        function adminPage(){
-            add_options_page(
-                'Posts Read Statics',
-                'Read Statics',
-                'manage_options',
-                'posts-read-statics',
-                array($this, 'pluginSettingsHTML')
-            );
-        }
+    function adminPage(){
+        add_options_page(
+            'Posts Read Statics',
+            'Read Statics',
+            'manage_options',
+            'posts-read-statics-settings',
+            array($this, 'pluginSettingsHTML')
+        );
+    }
+
+    function registerSettings(){
+        add_settings_section( 'prsFirstSection', null, null, 'posts-read-statics-settings');
+        add_settings_field('prs_location', 'Display Location', array($this, 'locationHTML'), 'posts-read-statics-settings', 'prsFirstSection');
+        register_setting('postsReadStatics', 'prs_location', array(
+            'sanitize_callback' => 'sanitize_text_field',
+                'default' => '0'
+        ));
+    }
+
+    function locationHTML(){ ?>
+<select name="prs_location">
+    <option value="0">Beginning of post</option>
+    <option value="1">End of post</option>
+</select>
+<?php   
+    }
         
-        function pluginSettingsHTML(){  ?>
+    function pluginSettingsHTML(){  ?>
 <div class="wrap">
     <h1>Posts Read Statics Settings</h1>
+    <form action="options.php" method="POST">
+        <?php
+            settings_fields('postsReadStatics'); 
+            do_settings_sections('posts-read-statics-settings'); 
+            submit_button();
+        ?>
+    </form>
 </div>
-<?php }
+<?php
+}
 }
 
 $readingStatics = new ReadingStatics();
