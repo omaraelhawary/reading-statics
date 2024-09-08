28e80ceea77faacb9cda3c590e23b6d80c71e678
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-08 21:11:31 +0300

    Add french files

---
+++ languages/prsdomain-fr_FR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'Posts Read Statics','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-09-08 18:08+0000','po-revision-date'=>'2024-09-08 18:11+0000','last-translator'=>'Admin','language-team'=>'French (France)','language'=>'fr_FR','plural-forms'=>'nplurals=2; plural=n > 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.11; wp-6.6.1','x-domain'=>'prsdomain','messages'=>['This Post has'=>'Cet article a','words'=>'mots']];
